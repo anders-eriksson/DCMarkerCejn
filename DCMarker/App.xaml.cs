d5ec90882a41896b967e80f03f9ef4c1d79ab54c
'andis59' <'anders@andis59.se'>
2017-02-14 13:27:44 +0100

    Added a reset of all IO signals Added DC Lasersystems Icon Added About Added a selection of which Window that should show. Depends on TypeOfMachine. Updated language resources Removed Monitor.Wait/Pulse from Laser Changed so that Workflow isn't started in a new thread!

--- DCMarker/App.xaml.cs
+++ DCMarker/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Configuration;
 using System.Windows;
 
 namespace DCMarker
@@ -13,5 +8,32 @@ namespace DCMarker
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Window wnd = null;
+
+            DCConfig cfg = DCConfig.Instance;
+            switch (cfg.TypeOfMachine)
+            {
+                case 1:
+                    wnd = new MainWindow();
+                    break;
+
+                //case 2:
+                //    wnd = new ManualMainWindow();
+                //    break;
+
+                default:
+                    wnd = new ErrorMainWindow();
+                    break;
+            }
+            // Create the startup window
+
+            // Show the window
+            if (wnd != null)
+            {
+                wnd.Show();
+            }
+        }
     }
 }
