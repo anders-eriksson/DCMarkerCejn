ca915ef2d5181367052a445ff9344d926c91b6f8
andis59 <anders@andis59.se>
2017-02-23 09:22:55 +0100

    Added logging
    Changed LaserData so it works with the new table changes

--- DCMarkerEF/LaserData.cs
+++ DCMarkerEF/LaserData.cs
@@ -15,6 +15,12 @@ namespace DCMarkerEF
         [StringLength(1)]
         public string Kant { get; set; }
 
+        [StringLength(100)]
+        public string Avdelning { get; set; }
+
+        [StringLength(100)]
+        public string Laser { get; set; }
+
         [StringLength(50)]
         public string F2 { get; set; }
 
